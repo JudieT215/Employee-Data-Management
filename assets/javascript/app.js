a79ec209c64b92a27e53a42a78988fc8f4428e1b
Tran <Tran.Judie@gmail.com>
2018-03-15 20:12:25 -0400

    fixed it maybe

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,32 +1,39 @@
 $(document).ready(function() {
-    var database = firebase.database();
-    
- 
-    console.log(database);
-        var name = "";
-        var role = "";
-        var age = 0;
-        var startDate = "";
-        var monthlyRate ="";
+  var database = firebase.database();
 
-$("#submitEmployee").on("click", function(event) {
-      event.preventDefault();
-      
-      name = $("#employeeName").val().trim();
-      role = $("#role").val().trim();
-      age = $("#age").val().trim();
-      startDate = $("#start-date").val().trim();
-      monthlyRate = $("#Monthly-date").val().trim();
+  console.log(database);
+  var name = "";
+  var role = "";
+  var age = 0;
+  var startDate = "";
+  var monthlyRate = "";
 
-});
-})
+  $("#submitEmployee").on("click", function(event) {
+    event.preventDefault();
 
-dataRef.ref().on("child_added", function(childSnapshot) {
-  // Log everything that's coming out of snapshot
-  console.log(childSnapshot.val().name);
-  console.log(childSnapshot.val().role);
-  console.log(childSnapshot.val().age);
-  console.log(childSnapshot.val().startDate);
-  console.log(childSnapshot.val().comment);
-  console.log(childSnapshot.val().joinDate);
+    name = $("#employeeName")
+      .val()
+      .trim();
+    role = $("#role")
+      .val()
+      .trim();
+    age = $("#age")
+      .val()
+      .trim();
+    startDate = $("#start-date")
+      .val()
+      .trim();
+    monthlyRate = $("#Monthly-date")
+      .val()
+      .trim();
+  });
+
+  dataRef.ref().on("child_added", function(childSnapshot) {
+    // Log everything that's coming out of snapshot
+    console.log(childSnapshot.val().name);
+    console.log(childSnapshot.val().role);
+    console.log(childSnapshot.val().age);
+    console.log(childSnapshot.val().startDate);
+    console.log(childSnapshot.val().monthlyRate);
+  });
 });

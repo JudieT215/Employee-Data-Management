21c6f9b93e2e51e4f40de3ede20732fe7e4e9b95
Tran <Tran.Judie@gmail.com>
2018-03-15 20:05:24 -0400

    add vars

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,21 @@
 $(document).ready(function() {
     var database = firebase.database();
+    
+ 
     console.log(database);
+        var name = "";
+        var role = "";
+        var age = 0;
+        var startDate = "";
+        var monthlyRate ="";
 
+$("#submitEmployee").on("click", function(event) {
+      event.preventDefault();
+      
+      name = $("#employeeName").val().trim();
+      role = $("#role").val().trim();
+      age = $("#age").val().trim();
+      startDate = $("#start-date").val().trim();
+      monthlyRate = $("#Monthly-date").val().trim();
 
 });
